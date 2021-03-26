4c1edbbf3251fa7ebce7c793b183f7e394c53562
LeeGangMin <godqwer123@naver.com>
2021-03-26 17:27:36 +0900

    setup settings(babel, nodemon)

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hi!");
+// const express = require('express');
+import express from "express";
+const app = express();
+
+const PORT = 5500;
+
+function handleListening() {
+    console.log(`Listening on : http://localhost:${PORT}`);
+}
+
+
+const handleHome = (req, res) => res.send("Hello from home");
+// function handleHome(req, res){
+//     // console.log('Hi from home!!');
+//     res.send("Hello from home");
+// }
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+// function handleProfile(req, res){
+//     res.send("You are on my profile");
+// }
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
